6451cd98b38da5bd64a293557b579bcbefee260f
ParfinovichOlga <parfinovich.olga@yandex.ru>
2025-04-09 10:40:26 +0300

    Updated main.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# async_parser
+# Async_parser
+Scrap data from spimex.com and store to a PostgreSQL database
+
+# Usage
+Create database in PostgreSQL and fill out your .env 
+file with credentials before start
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import datetime as dt
 import asyncio
+import aiofiles
 from aiohttp import ClientSession
 from dateutil import parser
 import pandas as pd
@@ -26,19 +27,19 @@ chrome_options.add_experimental_option('useAutomationExtension', False)
 driver = webdriver.Chrome(options=chrome_options)
 
 
-def get_file_date(file_url: str):
+def get_file_date(file_url: str) -> dt.datetime:
     """Retrieve date from file name."""
     date = parser.parse(file_url.split('_')[-1][:8]).date()
     return date
 
 
-async def produce(urls: list, q: asyncio.Queue):
-    """Collect links: Add urls to queue for processing"""
+async def produce(urls: list, q: asyncio.Queue) -> None:
+    """Collect links: Add urls to queue for processing."""
     for url in urls:
         await q.put(url)
 
 
-async def _get_links():
+async def _get_links() -> list:
     """Collect links: Get download links from the page."""
     link_obj = driver.find_elements(By.CSS_SELECTOR, LINK_CSS_SEL)
     return [
@@ -47,8 +48,8 @@ async def _get_links():
             ]
 
 
-async def grab_links(q: asyncio.Queue, url=START_URL):
-    """Collect links from url"""
+async def grab_links(q: asyncio.Queue, url=START_URL) -> asyncio.Queue:
+    """Collect links from url."""
     driver.get(url)
     str_obj = await _get_links()
     if str_obj:
@@ -59,52 +60,57 @@ async def grab_links(q: asyncio.Queue, url=START_URL):
     return q
 
 
-async def download_file(url: str, session: ClientSession):
+async def download_file(url: str, session: ClientSession) -> (str, dt.datetime):
     """Download file from url to server."""
     query_params = {'downloadformat': 'xlsx'}
     async with session.get(url, params=query_params) as response:
         date = get_file_date(url)
         name = f'{date}.xlsx'
+        response.raise_for_status()
         data = await response.read()
-        with open(name, "wb") as f:
-            f.write(data)
+        async with aiofiles.open(name, "wb") as f:
+            await f.write(data)
         return name, date
 
 
-async def retrieve_requested_data_from_file(url: str, session: ClientSession):
+async def retrieve_requested_data_from_file(url: str, session: ClientSession) -> pd.DataFrame:
     """Retrieve requested data from file."""
-    file_name, date = await download_file(url, session)
-    pre_data = pd.read_excel(file_name, usecols='B', nrows=20)
+    try:
+        file_name, date = await download_file(url, session)
+    except Exception as e:
+        print(e)
+    else:
+        pre_data = pd.read_excel(file_name, usecols='B', nrows=20)
 
-    row, col = np.where(pre_data == 'Единица измерения: Метрическая тонна')
+        row, col = np.where(pre_data == 'Единица измерения: Метрическая тонна')
 
-    data = pd.read_excel(file_name, usecols='B:F,O', skiprows=int(row[0])+2)
+        data = pd.read_excel(file_name, usecols='B:F,O', skiprows=int(row[0])+2)
 
-    data.rename(columns={'Код\nИнструмента': 'exchange_product_id',
-                         'Наименование\nИнструмента': 'exchange_product_name',
-                         'Базис\nпоставки': 'delivery_basis_name',
-                         'Объем\nДоговоров\nв единицах\nизмерения': 'volume',
-                         'Обьем\nДоговоров,\nруб.': 'total',
-                         'Количество\nДоговоров,\nшт.': 'count'
-                         }, inplace=True)
+        data.rename(columns={'Код\nИнструмента': 'exchange_product_id',
+                             'Наименование\nИнструмента': 'exchange_product_name',
+                             'Базис\nпоставки': 'delivery_basis_name',
+                             'Объем\nДоговоров\nв единицах\nизмерения': 'volume',
+                             'Обьем\nДоговоров,\nруб.': 'total',
+                             'Количество\nДоговоров,\nшт.': 'count'
+                             }, inplace=True)
 
-    data = data.dropna()
-    data.insert(2, 'oil_id', data['exchange_product_id'].str[:4])
-    data.insert(3, 'delivery_basis_id', data['exchange_product_id'].str[4:7])
-    data.insert(5, 'delivery_type_id', data['exchange_product_id'].str[-1:])
-    data['date'] = date
-    clean_data = data[data['count'] != '-']
-    await delete_file(file_name)
-    return clean_data
+        data = data.dropna()
+        data.insert(2, 'oil_id', data['exchange_product_id'].str[:4])
+        data.insert(3, 'delivery_basis_id', data['exchange_product_id'].str[4:7])
+        data.insert(5, 'delivery_type_id', data['exchange_product_id'].str[-1:])
+        data['date'] = date
+        clean_data = data[data['count'] != '-']
+        await delete_file(file_name)
+        return clean_data
 
 
-async def delete_file(file):
+async def delete_file(file: str):
     """Delete file from server."""
     os.remove(file)
 
 
 async def add_to_db(work_queue: asyncio.Queue, session: ClientSession):
-    """Add data to database"""
+    """Add data to database."""
     while not work_queue.empty():
         url = await work_queue.get()
         data = await retrieve_requested_data_from_file(url, session)
@@ -112,6 +118,10 @@ async def add_to_db(work_queue: asyncio.Queue, session: ClientSession):
 
 
 async def main(n=10):
+    """
+    Process inserting data to database.
+    n - number of tasks
+    """
     work_queue = asyncio.Queue()
     await grab_links(work_queue)
     await orm.create_tables()
